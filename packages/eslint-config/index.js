c1374500b9ea8cf2a9619951c325262ef8a710f7
Arafat Zahan <kuasha420@gmail.com>
2026-03-04 13:44:44 +0600

    chore: setup turborepo, pnpm workspaces, and core configs

---
+++ packages/eslint-config/index.js
@@ -0,0 +1,18 @@
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  env: {
+    node: true,
+  },
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  rules: {
+    "@typescript-eslint/no-unused-vars": "error",
+    "@typescript-eslint/no-explicit-any": "warn",
+  },
+  ignorePatterns: ["dist/", ".turbo/", "node_modules/"],
+};
